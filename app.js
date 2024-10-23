5671bfd2912841ced000206e6c871420898045d1
CrisEvaristo <cristovaoevaristomanuel@gmail.com>
2024-10-23 06:11:15 +0100

    Adicionando o arquivo nodemon e criando rotas com express

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("olá mundo");
+//Importar o módulo express
+
+const express = require('express');
+
+//app
+
+const app = express();
+
+//Rota ola mundo
+
+app.get('/', function(req,res){
+    res.write("Utilizando nodermon");
+    res.end();
+});
+
+//servidor
+
+app.listen(8080);
